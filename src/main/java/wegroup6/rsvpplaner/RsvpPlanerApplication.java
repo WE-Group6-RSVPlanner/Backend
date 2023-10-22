84b6860dd26e817d7dcec20dcafafe47297ff0c8
Eren Dogan <ubuntu@pc.localdomain>
2023-10-22 11:37:46 +0200

    init

---
+++ src/main/java/wegroup6/rsvpplaner/RsvpPlanerApplication.java
@@ -0,0 +1,13 @@
+package wegroup6.rsvpplaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsvpPlanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsvpPlanerApplication.class, args);
+	}
+
+}
